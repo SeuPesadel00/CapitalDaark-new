e601ec80b4532ed3ee62a70407954947d47e67bd
Pesadel00 <arthur.hhee@gmail.com>
2026-08-26 00:15:42 -0300

    fix: remove CartProvider that was breaking vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
-import { CartProvider } from './context/CartContext';
 import { Toaster } from '@/components/ui/toaster';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <CartProvider>
         <App />
         <Toaster />
-      </CartProvider>
     </AuthProvider>
   </StrictMode>,
 );
